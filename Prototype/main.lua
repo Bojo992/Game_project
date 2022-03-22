49291033359488332865570af17472662aa2f594
Boris Bobylkov <mm22mm44bbi@gmail.com>
2022-03-22 21:23:03 +0000

    Add enemy shooting function, bullet colision. Started working on miss shots and death for protagonist.

--- Prototype/main.lua
+++ Prototype/main.lua
@@ -3,46 +3,95 @@ physics.start()
 
 local score = 0;
 local scoreText = display.newText( score, 300, display.contentCenterY, native.systemFont, 16 )
-
+local sign
+local miss = 0
 --
 --Characters
 
 local protagonist = display.newRect(100, 20, 20, 20)
 protagonist.fill = {0, 150, 255}
 physics.addBody(protagonist, "static")
+protagonist.myName = "protagonist"
 
 local antaganist = display.newRect(100, 520, 20, 20)
 antaganist.fill = {255, 0, 0}
 physics.addBody(antaganist, "static")
+antaganist.myName = "antaganist"
 
 --
 --Bullet
-local bullet
+local bullet = display.newRect(-100, 200, 30, 30)
+local bulletEnemy = display.newRect(0, -5, 0, 0)
+
+
+local counter = 0
+local enemySootingTime = math.random(30, 60)
 
 function shoot(event)
-    if event.phase == "began" then
-        bullet = display.newRect(100, 40, 5, 20)
-        physics.addBody(bullet, "dynamic", { isSensor=true })
-        bullet.isBullet = true
+    if (event.phase == "began") and (counter >= enemySootingTime - 10) then
+        bullet = display.newRect(100, 50, 5, 20)
+        physics.addBody(bullet, "dynamic", {radius = 20, isSensor=true })
+        bullet.myName = "bullet"
         transition.to(bullet, {y = 525, time = 525,
         onComplete = function() display.remove( bullet ) end
         })
+    else 
+        display.newText( "MISS", display.contentCenterX, display.contentCenterY, native.systemFont, 40 )
+        miss = 1;
     end
 end
+
+function shootEnemy()
+    if (counter == enemySootingTime) then
+        sign = display.newRect(300, 300, 100, 10)
+    end
+
+    if (counter == enemySootingTime+120) then
+        display.remove(sign)
+    end
+    
+    if (counter == enemySootingTime+60) and (score == 0) then
+        bulletEnemy = display.newRect(100, 490, 5, 20)
+        physics.addBody(bulletEnemy, "dinamick", {radius = 20, isSensor=true })
+        bulletEnemy.myName = "bulletEnemy"
+        transition.to(bulletEnemy, {y = 20, time = 525,
+        onComplete = function() display.remove( bulletEnemy ) end
+        })
+    end
+    counter = counter + 1
+end
+
+
+Runtime:addEventListener("enterFrame", shootEnemy)
 
 Runtime:addEventListener("touch", shoot)
 
 --
 --Collision
 
-local function onLocalCollision( self, event )
- 
+local function onLocalCollision( event )
     if ( event.phase == "began" ) then
-        score = score + 1
-        scoreText.text = score
-        bullet:toBack()
+        local obj1 = event.object1 
+        local obj2 = event.object2 
+        print("obj1: " .. obj1.myName)
+        print("obj2: " .. obj2.myName)
+
+        if ((obj1.myName == "bullet" and obj2.myName == "bulletEnemy") or 
+            (obj1.myName == "bulletEnemy" and obj2.myName == "bullet")) then
+            display.remove( obj1 )
+            display.remove( obj2 )
+            counter = 0
+        end
+
+        if ((obj1.myName == "bullet" and obj2.myName == "antaganist") or 
+            (obj1.myName == "bulletEnemy" and obj2.myName == "protagonist") or
+            (obj1.myName == "antaganist" and obj2.myName == "bullet") or 
+            (obj1.myName == "protagonist" and obj2.myName == "bulletEnemy"))
+        then
+            score = score + 1
+            scoreText.text = score
+        end
     end
 end
- 
-antaganist.collision = onLocalCollision
-antaganist:addEventListener( "collision" )
+
+Runtime:addEventListener("collision", onLocalCollision)
