b8a1167a4d204dc78ea80fc7cee6168d19f17307
DESKTOP-HOEUPFM\mm22m <mm22mm44bbi@gmail.com>
2022-04-16 13:44:29 +0100

    Created baseClass

--- Prototype/main.lua
+++ Prototype/main.lua
@@ -1,140 +1,50 @@
-local x,y = display.contentCenterX, display.contentCenterY
-local o = display.newRect( x, y, display.contentWidth, display.contentHeight )
-o.fill = { type="image", filename="Backgrounds\\Lv8 Richolas.png" }
---
---Util variables
-local physics = require("physics")
-physics.start()
+require ("baseClass")
 
-local score = 0;
+lifes = 1
+level = 2
+enemyShootAnimation = "Enemy"..level.."_shoot"
 
-lifes = 2
-local sign
-local miss = 0
-local counter = 0
-local openingWondowForShot = math.random(30, 60)
-local closingWindowForShot = openingWondowForShot + 60
+background("Backgrounds\\Lv"..level..".png")
 
---
---Characters
--- local protagonist = display.newRect(100, display.contentCenterY+100, 50, 50)
--- protagonist.alpha = 0;
-local protagonist = display.newImage("Sprites\\Big Richard\\Bigger_Richard.png",100, display.contentCenterY+100, 20, 20)
-physics.addBody(protagonist, "static", {radius = 20, isSensor=true })
-protagonist.myName = "protagonist"
+protagonist("BR_idle")
+antagonist("Enemy"..level.."_idle")
 
-local antaganist = display.newImage("Sprites\\Enemies\\Big Banker\\Big_Banker_Idle.png", 540, display.contentCenterY+100, 20, 20)
-physics.addBody(antaganist, "static")
-antaganist.myName = "antaganist"
-
---
---Bullets
-local bullet
-
---
---Controls player's shooting and correctness of those
-    function shoot(event)
-        if (event.phase == "began") then
-            if (counter >= openingWondowForShot) and (counter <= closingWindowForShot) and (miss == 0) then
-                --Shooting at right time
-                bullet = display.newRect(120, display.contentCenterY+100, 20, 5)
-                physics.addBody(bullet, "dynamic", {radius = 20, isSensor=true })
-                bullet.myName = "bullet"
-                physics.addBody(antaganist, "static", {radius = 20, isSensor=true })
-                transition.to(bullet, {x = 560, time = 300,
-                onComplete = function() display.remove( bullet ) end
-                })
-            else 
-                --Miss shot
-                print("miss: ".. miss)
-                miss = 1;
-            end
-        end
-    end
-
---
---Controls enemy's shooting and sign    
-    function shootEnemy()
-        --Sign
-        if (counter == openingWondowForShot) then
-            display.remove(antaganist)
-            antaganist = display.newImage("Sprites\\Enemies\\Big Banker\\Big_Banker_Shoot_F3.png", 540, display.contentCenterY+100, 20, 20)
-            antaganist.myName = "antaganist"
-            sign = display.newImage("Sprites\\Objects\\Fire!!.png", display.contentCenterX, display.contentCenterY)
-        end
-
-        if (counter == closingWindowForShot) then
-            display.remove(sign)
-        end
-        
-        --Enemy shooting
-        if (counter == closingWindowForShot) and (score == 0) then
-            display.remove(antaganist)
-            antaganist = display.newImage("Sprites\\Enemies\\Big Banker\\Big_Banker_Shoot_F1.png", 540, display.contentCenterY+100, 20, 20)
-            antaganist.myName = "antaganist"
-            local bulletEnemy = display.newRect(540, y + 100, 20, 5)
-            physics.addBody(bulletEnemy, "dynamic", {isSensor = true})
-            bulletEnemy.gravityScale = 0
-            bulletEnemy.myName = "bulletEnemy"
-            transition.to(bulletEnemy, {x = -100, time = 525,
-            onComplete = function() display.remove(bulletEnemy) end
-            })
-        end
-
-        counter = counter + 1
-    end
-
-
-Runtime:addEventListener("enterFrame", shootEnemy)
-Runtime:addEventListener("touch", shoot)
-
---
---Collision
-
-local function onLocalCollision(event)
+function onLocalCollision(event)
     if (event.phase == "began") then
         local obj1 = event.object1 
         local obj2 = event.object2 
+
         print("obj1: " .. obj1.myName)
         print("obj2: " .. obj2.myName)
 
         --Successfull shot
-        if ((obj1 == bullet and obj2 == antaganist) or 
-            (obj1 == antaganist and obj2 == bullet))
+        if ((obj1.myName == "missile" and obj2.myName == "antagonist") or 
+            (obj1.myName == "antagonist" and obj2.myName == "missile"))
         then
             lifes = lifes - 1
+
             if (lifes == 0)
             then
                 score = 1
-                display.remove(antaganist)
-                antaganist = display.newImage("Sprites\\Enemies\\Revolver Richolas\\Revolver_Richolas_Dead.png", 540, display.contentCenterY+100, 20, 20)
+                display.remove(sign)
+                antagonistChange("Enemy"..level.."_die")
             else
-                display.remove(antaganist)
-                antaganist = display.newImage("Sprites\\Enemies\\Big Banker\\Big_Banker_ShotOnce.png", 540, display.contentCenterY+100, 20, 20)
-                antaganist.myName = "antaganist"
-                transition.to(antaganist, {x = 541, time = 525,
-                onComplete = 
-                function()
-                    display.remove(antaganist)
-                    antaganist = display.newImage("Sprites\\Enemies\\Big Banker\\Big_Banker_Idle_2ndPhase.png", 540, display.contentCenterY+100, 20, 20)
-                    antaganist.myName = "antaganist"
-                end
-                })
+                antagonistChange("Enemy"..level.."_"..lifes.."die")
                 display.remove(sign)
                 counter = 0
-
             end
         end
 
         if ((obj1.myName == "bulletEnemy" and obj2.myName == "protagonist") or 
             (obj1.myName == "protagonist" and obj2.myName == "bulletEnemy"))
         then
-            display.remove(protagonist)
-            protagonist = display.newImage("Sprites\\Big Richard\\Big_Richard_Die1_F4.png",100, display.contentCenterY+100, 20, 20)
-            antaganist:toFront()
+            protagonistChange("BR_Die"..level)
+            antagonist:toFront()
             miss = 2
         end
     end
 end
 
+Runtime:addEventListener("enterFrame", shootEnemy)
+Runtime:addEventListener("touch", shoot)
 Runtime:addEventListener("collision", onLocalCollision)
